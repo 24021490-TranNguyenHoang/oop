f2f723af5d94a6acb5ff122aef7d9dd3a99817df
Trần Hoàng <hoangcamly150906@gmail.com>
2025-09-20 22:02:32 +0700

    First clean commit

---
+++ oop-main/HW1/Introduction.java
@@ -0,0 +1,19 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Họ và tên: Trần Nguyễn Hoàng");
+        System.out.println("Mã sinh viên: 24021490");
+        System.out.println("Lớp: CS5");
+        System.out.println("GitHub username: 24021490-TranNguyenHoang");
+        System.out.println("Email: 24021490@vnu.edu.vn");
+        System.out.println();
+
+        for (int i = 9; i >= 2; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
